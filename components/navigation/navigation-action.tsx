cc449f005a815525d753b70e2ed3bb2ac6a7eb4b
Rhys <2068188418@qq.com>
2023-10-09 16:21:48 +0800

    navigation sidebar

---
+++ components/navigation/navigation-action.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Plus } from "lucide-react";
+
+import ActionTooltip from "@/components/action-tooltip";
+
+const NavigationAction = () => {
+  return (
+    <div>
+      <ActionTooltip side="right" align="center" laber="Add a server">
+        <button className="group flex items-center">
+          <div
+            className="flex mx-3 h-[48px] w-[48px] rounded-[24px] 
+            group-hover:rounded-[16px] overflow-hidden items-center
+            justify-center bg-background dark:bg-neutral-700 
+            group-hover:bg-emerald-500"
+          >
+            <Plus
+              size={25}
+              className="group-hover:text-white transition-all text-emerald-500"
+            ></Plus>
+          </div>
+        </button>
+      </ActionTooltip>
+    </div>
+  );
+};
+
+export default NavigationAction;
--- components/toggle.tsx
+++ components/toggle.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { cn } from "@/lib/utils";
 import type { MenuProps } from "antd";
 import { Dropdown } from "antd";
