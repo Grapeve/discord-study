02aca0d26e9fc00908da88b27dcdf2879eeb6a81
Rhys <2068188418@qq.com>
2023-10-10 01:45:23 +0800

    Invitation: invite others to join the server

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -19,6 +19,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
   server: ServerWithMembersWithProfiles;
@@ -26,6 +27,8 @@ interface ServerHeaderProps {
 }
 
 const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal();
+
   const isAdmin = role === MemberRole.ADMIN;
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -44,6 +47,7 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
           <DropdownMenuItem
+            onClick={() => onOpen("invite", { server: server })}
             className="text-indigo-600 dark:text-indigo-400
             px-3 py-2 text-sm cursor-pointer"
           >
