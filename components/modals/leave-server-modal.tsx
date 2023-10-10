89d19cfd7831717394f1b537dfac354b21b7f75a
Rhys <2068188418@qq.com>
2023-10-10 19:46:44 +0800

    feat: delete and leave server modal

---
+++ components/modals/leave-server-modal.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { useModal } from "@/hooks/use-modal-store";
+
+const LeaveServerModal = () => {
+  const router = useRouter();
+  const { isOpen, onClose, type, data } = useModal();
+
+  const isModalOpen = isOpen && type === "leaveServer";
+  const { server } = data;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.patch(`/api/servers/${server?.id}/leave`);
+
+      onClose();
+      router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <Dialog open={isModalOpen} onOpenChange={onClose}>
+        <DialogContent className="bg-white text-black p-0 overflow-hidden">
+          <DialogHeader className=" pt-8 px-6">
+            <DialogTitle className="text-2xl text-center font-bold">
+              Leaver Server
+            </DialogTitle>
+            <DialogDescription className="text-center text-zinc-500">
+              Are you sure you want to leave{" "}
+              <span className="font-semibold text-indigo-500">
+                {server?.name}
+              </span>{" "}
+              server?
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter className="bg-gray-100 px-6 py-4">
+            <div className="flex items-center justify-between w-full">
+              <Button disabled={isLoading} onClick={onClose} variant="ghost">
+                Cannel
+              </Button>
+              <Button disabled={isLoading} onClick={onClick} variant="primary">
+                Confirm
+              </Button>
+            </div>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default LeaveServerModal;
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -7,6 +7,8 @@ import InviteModal from "@/components/modals/invite-modal";
 import EditServerModal from "@/components/modals/edit-server-modal";
 import MembersModal from "@/components/modals/members-modal";
 import CreateChannelModal from "@/components/modals/create-channel-modal copy";
+import LeaveServerModal from "@/components/modals/leave-server-modal";
+import DeleteServerModal from "@/components/modals/delete-server-modal";
 
 const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -26,6 +28,8 @@ const ModalProvider = () => {
       <EditServerModal />
       <MembersModal />
       <CreateChannelModal />
+      <LeaveServerModal />
+      <DeleteServerModal />
     </>
   );
 };
